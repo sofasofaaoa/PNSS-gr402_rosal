6973ce5d56defba8e7c17c35300b261c44473338
sofasofaaoa <ladyrobertus3@gmail.com>
2023-03-27 07:22:43 +0300

    v1dimalox

---
+++ PNSS-gr402_rosal/app/Controller/Site.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controller;
+
+use Src\View;
+
+class Site
+{
+    public function index(): string
+    {
+        $view = new View();
+        return $view->render('site.hello', ['message' => 'index working']);
+    }
+
+    public function hello(): string
+    {
+        return new View('site.hello', ['message' => 'hello working']);
+    }
+}
---
+++ PNSS-gr402_rosal/core/Src/Application.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Src;
+
+use Error;
+
+class Application
+{
+    private Settings $settings;
+    private Route $route;
+
+    public function __construct(Settings $settings)
+    {
+        $this->settings = $settings;
+        $this->route = new Route();
+    }
+
+    public function __get($key)
+    {
+        if ($key === 'settings') {
+            return $this->settings;
+        }
+        throw new Error('Accessing a non-existent property');
+    }
+
+    public function run(): void
+    {
+        $this->route->setPrefix($this->settings->getRootPath());
+        $this->route->start();
+    }
+}
