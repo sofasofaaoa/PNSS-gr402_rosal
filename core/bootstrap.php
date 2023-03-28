378185f199563435443f0535c2197b25b68f0d80
sofasofaaoa <ladyrobertus3@gmail.com>
2023-03-28 16:21:08 +0700

    Git and Composer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,18 +2,8 @@
 //Путь до директории с конфигурационными файлами
 const DIR_CONFIG = '/../config';
 
-//Добавляем пользовательскую функцию автозагрузки классов
-spl_autoload_register(function ($className) {
-    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
-    $className = str_replace('\\', '/', $className);
-
-    foreach ($paths['classes'] as $path) {
-        $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
-        if (file_exists($fileName)) {
-            require_once $fileName;
-        }
-    }
-});
+//Подключение автозагрузчика composer
+require_once __DIR__ . '/../vendor/autoload.php';
 
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
