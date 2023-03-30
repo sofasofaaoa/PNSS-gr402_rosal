992cea3d641fd398cdc933347db2d6b491f8f2d0
sofasofaaoa <ladyrobertus3@gmail.com>
2023-03-30 19:58:08 +0700

    Middleware

--- config/app.php
+++ config/app.php
@@ -3,5 +3,9 @@ return [
     //Класс аутентификации
     'auth' => \Src\Auth\Auth::class,
     //Клас пользователя
-    'identity'=>\Model\User::class
+    'identity' => \Model\User::class,
+    //Классы для middleware
+    'routeMiddleware' => [
+        'auth' => \Middlewares\AuthMiddleware::class,
+    ]
 ];
